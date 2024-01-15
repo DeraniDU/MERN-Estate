6e00790b9242884eb524cb0d7f65bcb3919032f3
Deranindu Gunasekara <it22585844@my.sliit.lk>
2024-01-15 20:10:34 +0530

    create and run the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
 
-export default function App() {
-  return (
-    <h1 className='text-red-500'>App</h1>
-  )
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import Signup from './pages/SignUp';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import Header from './Components/Header';
+
+export default function App(){
+
+  return<BrowserRouter>
+  <Header/>
+  
+  <Routes>
+
+    <Route path="/" element={<Home/>} />
+    <Route path="/Sign-In" element={<SignIn />} />
+    <Route path="/Sign-Up" element={<Signup />} />
+    <Route path="/Profile" element={<Profile />} />
+    <Route path="/About" element={<About />} />
+  </Routes>
+
+
+  </BrowserRouter>
+
+
 }
+
+
+
+  
+
+
+
+/*import React from "react";
+
+export default function App(){
+    return<h1 className='text-red-500'>App </h1>
+
+}*/
